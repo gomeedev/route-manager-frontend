64a9e4a55ac2a52d93d889ab818ede4b4c6b5741
Johann <johanns.gomeze@gmail.com>
2025-10-30 20:58:42 -0500

    feat: Agregar configuracion del tailwind.config.js

--- src/pages/authPages/SignUpPage.jsx
+++ src/pages/authPages/SignUpPage.jsx
@@ -2,6 +2,9 @@ import react, { useState } from "react"
 
 
 export const SignUpPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
 
     return <h1>Registrarme</h1>
 }
