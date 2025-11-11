f6333c4c199fd434fc635dd1a32a48745ecf000f
Johann <johanns.gomeze@gmail.com>
2025-11-10 19:50:33 -0500

    feat: Implementar formateador de fecha en usuario

--- src/components/drivers/profile/MostrarPerfilDriver.jsx
+++ src/components/drivers/profile/MostrarPerfilDriver.jsx
@@ -5,6 +5,15 @@ export const MostrarPerfilDriver = () => {
 
     let user = JSON.parse(localStorage.getItem("user"))
 
+    const fechaBackend = user.fecha_actualizacion_foto
+    const fecha = new Date(fechaBackend)
+    const formatoFecha = new Intl.DateTimeFormat("es-CO", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    }).format(fecha)
+
     switch (user.tipo_documento) {
         case "CC":
             user.tipo_documento = "Cédula de ciudadania"
@@ -20,7 +29,7 @@ export const MostrarPerfilDriver = () => {
         <>
             <ComponentCard title={<><p>Perteneces a la empresa <i>{user.empresa_nombre}</i></p></>}>
                 <img src={user.foto_perfil || fotoDefaultUrl} alt="Foto de perfil" className="rounded-[50%] w-[100px] h-[100px]" />
-                <p>Ultima actualización: <i>{user.fecha_actualizacion_foto || "No has subido foto"}</i></p>
+                <p>Ultima actualización: <i>{formatoFecha || "No has subido foto"}</i></p>
                 <p>Estado: <i>{user.estado}</i></p>
                 <p>Rol: <i>{user.rol_nombre}</i></p> <br />
                 <p>Nombre: <i>{user.nombre}</i></p>
