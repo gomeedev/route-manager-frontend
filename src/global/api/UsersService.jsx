b4baffc3e61692e521511c1d12c14c240ee1ddb2
Johann <johanns.gomeze@gmail.com>
2025-11-10 22:47:15 -0500

    feat: Implementar peticion para edicion de usuarios

--- src/global/api/UsersService.jsx
+++ src/global/api/UsersService.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 export const SigninUserSupabase = async (email, password) => {
 
-    const {data, error} = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
         email,
         password,
     })
@@ -17,10 +17,27 @@ export const SigninUserSupabase = async (email, password) => {
 
 
 export const SigninUserDjango = async (token) => {
-    
+
     const response = await axios.get(`${API_URL}/api/v1/usuario/me`, {
-        headers: {"Authorization": `Bearer ${token}`}
+        headers: { "Authorization": `Bearer ${token}` }
     })
 
     return response.data
+}
+
+
+// Editar info de usuarios
+export const EditProfileUser = async (UserId, UserData) => {
+    try {
+        const response = await axios.patch(`${API_URL}/api/v1/usuario/${UserId}/`, UserData,
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        )
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
